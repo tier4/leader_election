192fd9b4997c75e17e1085385e218eb91ce6b399
Benjamin Gilby <gilbybenjamin@gmail.com>
2024-03-25 17:06:26 +0900

    push code heartbeat working on laptop

--- implementations/udp-linux/src/coordination.c
+++ implementations/udp-linux/src/coordination.c
@@ -48,7 +48,13 @@ double get_elapsed_time_ms(struct timeval start)
 void *handle_heartbeat(void *void_data)
 {
     char *data = (char *)void_data;
-    printf("Received heartbeat from node %s\n", data);
+    int sender_id = strtol(data, NULL, 10);
+
+    pthread_mutex_lock(&this_node.mu);
+    gettimeofday(&this_node.peers[sender_id].timeout_start, NULL);
+    pthread_mutex_unlock(&this_node.mu);
+
+    printf("Received heartbeat from node %d\n", sender_id);
     return 0;
 }
 
@@ -74,19 +80,20 @@ int send_until(const char *address, const char *port, const struct addrinfo *hin
 
     if ((status = getaddrinfo(address, port, hints, &serverinfo)) != 0)
     {
-        fprintf(stderr, "Error with getting address info, status = %s\n", gai_strerror(status));
+        fprintf(stderr, "Error with getting address info (send), status = %s\n", gai_strerror(status));
         return -1;
     }
 
     int sockfd;
     if ((sockfd = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol)) == -1)
     {
-        fprintf(stderr, "Error with getting socket file descriptor\n");
+        fprintf(stderr, "Error with getting socket file descriptor (send)\n");
         freeaddrinfo(serverinfo);
         return -1;
     }
 
     char msg[256]; // TODO: make this limit safe
+    // memset(msg, 0, 256);
     sprintf(msg, "%d", this_node.id);
     pthread_mutex_lock(mu);
     while (!*condition)
@@ -113,15 +120,17 @@ int recv_until(const char *address, const char *port, const struct addrinfo *hin
     int status;
     struct addrinfo *serverinfo;
 
+    fprintf(stderr, "calling getaddrinfo(%s, %s, hints, &serverinfo)\n", address, port);
     if ((status = getaddrinfo(address, port, hints, &serverinfo)) != 0)
     {
-        fprintf(stderr, "Error with getting address info, status = %s\n", gai_strerror(status));
+        fprintf(stderr, "Error with getting address info (receive), status = %s\n", gai_strerror(status));
+        return status;
     }
 
     int sockfd;
     if ((sockfd = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol)) == -1)
     {
-        fprintf(stderr, "Error with getting socket file descriptor\n");
+        fprintf(stderr, "Error with getting socket file descriptor (receive)\n");
         freeaddrinfo(serverinfo);
         return -1;
     }
@@ -203,7 +212,7 @@ void *start_hb_timers()
             }
         }
         pthread_mutex_unlock(&this_node.mu);
-        sleep(0.01); // check every 100ms, adjust this as needed
+        sleep(1); // check every 100ms, adjust this as needed
         pthread_mutex_lock(&this_node.mu);
     }
     pthread_mutex_unlock(&this_node.mu);
@@ -219,9 +228,11 @@ int begin_coordination(int num_nodes, int my_id)
     // for each other node, spinoff thread sending periodic heartbeats
     int hb_con = 0;
     pthread_mutex_t hb_mu;
+    pthread_mutex_init(&hb_mu, NULL);
     pthread_t hb_threads[num_nodes - 1];
     int hb_threads_count = 0;
     struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
 
@@ -230,13 +241,13 @@ int begin_coordination(int num_nodes, int my_id)
     {
         if (this_node.peers[i].id == my_id) // don't send message to oneself
             continue;
-        struct udpinfo send_args; // TODO: make sure this is allocate on heap so it isn't deallocated while thread is still running
-        send_args.address = this_node.peers[i].address;
-        send_args.port = this_node.peers[i].port;
-        send_args.hints = &hints;
-        send_args.condition = &hb_con;
-        send_args.mutex = &hb_mu;
-        pthread_create(&hb_threads[hb_threads_count++], NULL, send_until_pthread, &send_args);
+        struct udpinfo *send_args = (struct udpinfo *)malloc(sizeof(struct udpinfo));
+        send_args->address = this_node.peers[i].address;
+        send_args->port = this_node.peers[i].port;
+        send_args->hints = &hints;
+        send_args->condition = &hb_con;
+        send_args->mutex = &hb_mu;
+        pthread_create(&hb_threads[hb_threads_count++], NULL, send_until_pthread, send_args);
     }
     pthread_mutex_unlock(&this_node.mu);
 
@@ -244,24 +255,28 @@ int begin_coordination(int num_nodes, int my_id)
     int recv_con = 0;
     pthread_t recv_thread;
     pthread_mutex_t recv_mu;
+    pthread_mutex_init(&recv_mu, NULL);
     struct addrinfo recv_hints;
     memset(&recv_hints, 0, sizeof(recv_hints));
     recv_hints.ai_family = AF_INET;
     recv_hints.ai_socktype = SOCK_DGRAM;
+    // recv_hints.ai_flags = AI_PASSIVE;
 
     // recv_until(nodes[my_id].address, nodes[my_id].port, &recv_hints, &recv_con, &recv_mu);
 
-    struct udpinfo recv_args;
+    struct udpinfo *recv_args = (struct udpinfo *)malloc(sizeof(struct udpinfo));
 
     pthread_mutex_lock(&this_node.mu);
-    recv_args.address = this_node.peers[my_id].address;
-    recv_args.port = this_node.peers[my_id].port;
+    recv_args->address = this_node.peers[my_id].address;
+    // fprintf(stderr, "this_node.peers[my_id].address = %s\n", this_node.peers[my_id].address);
+    // recv_args->address = NULL;
+    recv_args->port = this_node.peers[my_id].port;
     pthread_mutex_unlock(&this_node.mu);
 
-    recv_args.hints = &recv_hints;
-    recv_args.condition = &recv_con;
-    recv_args.mutex = &recv_mu;
-    pthread_create(&recv_thread, NULL, recv_until_pthread, &recv_args);
+    recv_args->hints = &recv_hints;
+    recv_args->condition = &recv_con;
+    recv_args->mutex = &recv_mu;
+    pthread_create(&recv_thread, NULL, recv_until_pthread, recv_args);
 
     sleep(60 * 5); // sleep for 5 minutes so I can test heartbeats
     return 0;
@@ -272,8 +287,12 @@ int main(int argc, char **argv)
     // argv should be [number_of_nodes, node_info_file, my_node_id]
     if (argc != 4)
     {
-        fprintf(stderr, "Error: expected 3 command line arguments, found: %d", argc);
+        fprintf(stderr, "Error: expected 3 command line arguments (number of nodes, node info file, my node id), found: %d\n", argc - 1);
         exit(1);
+    }
+    else
+    {
+        printf("Running program with args: %s %s %s", argv[1], argv[2], argv[3]);
     }
 
     // get number of nodes from command line args
@@ -286,17 +305,29 @@ int main(int argc, char **argv)
     id address port
     ...
     */
-    FILE *node_info_file = fopen(argv[2], "r");
+    FILE *node_info_file;
+    if ((node_info_file = fopen(argv[2], "r")) == NULL)
+    {
+        fprintf(stderr, "Error: no such file or directory %s\n", argv[2]);
+        exit(1);
+    }
     struct peer_info peers[num_nodes];
     for (int i = 0; i < num_nodes; i++)
     {
-        if (fscanf(node_info_file, "%d %s %s", &peers[i].id, peers[i].address, peers[i].port) == EOF)
+        peers[i].address = (char *)malloc(16); // assume IPv4 addresses (15 chars max)
+        memset(peers[i].address, 0, 16);
+        peers[i].port = (char *)malloc(16);
+        memset(peers[i].port, 0, 16);
+        if (fscanf(node_info_file, "%d %15s %15s", &peers[i].id, peers[i].address, peers[i].port) == EOF)
         {
             fprintf(stderr, "Error: unexpected end of file\n");
+            fclose(node_info_file);
             exit(1);
         }
     }
     fclose(node_info_file);
+
+    pthread_mutex_init(&this_node.mu, NULL);
     this_node.peers = peers;
     this_node.num_nodes = num_nodes;
 
